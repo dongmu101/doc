bc227cfa5518bddf6349a9107e62577660dfed8d
erdong01 <erdong88155745@outlook.com>
2026-01-15 11:17:05 +0800

    fix

--- go/leetcode/88/run_test.go
+++ go/leetcode/88/run_test.go
@@ -14,20 +14,20 @@ func TestXxx(t *testing.T) {
 	fmt.Println(nums1)
 }
 func merge(nums1 []int, m int, nums2 []int, n int) {
-	for i, j, tail := m-1, n-1, m+n-1; i >= 0 || j >= 0; tail-- {
+	for p1, p2, tail := m-1, n-1, m+n-1; p1 >= 0 || p2 >= 0; tail-- {
 		var cur int
-		if i == -1 {
-			cur = nums2[j]
-			j--
-		} else if j == -1 {
-			cur = nums1[i]
-			i--
-		} else if nums1[i] > nums2[j] {
-			cur = nums1[i]
-			i--
+		if p1 == -1 {
+			cur = nums2[p2]
+			p2--
+		} else if p2 == -1 {
+			cur = nums1[p1]
+			p1--
+		} else if nums1[p1] > nums2[p2] {
+			cur = nums1[p1]
+			p1--
 		} else {
-			cur = nums2[j]
-			j--
+			cur = nums2[p2]
+			p2--
 		}
 		nums1[tail] = cur
 	}
